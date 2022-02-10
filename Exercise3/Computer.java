92ec07f4d28b13deb9fe640910b452cdb3a79f55
Игорь <bromegabob@gmail.com>
2022-02-10 22:25:32 +0200

    HomeWorks1

--- Exercise3/Computer.java
+++ Exercise3/Computer.java
@@ -1,36 +1,26 @@
 package Exercise3;
 
-public class Computer {
+import java.util.Arrays;
+
+public class Computer{
+    private int id;
+
+    public Computer(int id) {
+        this.id = id;
+    }
+
     public static void main(String[] args) {
+        Computer[] computers = new Computer[5];
 
+        for (int i = 0; i < computers.length; i++){
+            computers[i] = new Computer(i);
+        }
+        System.out.println(Arrays.toString(computers));
+    }
 
-        int[] computers = {5};
-
-
-        class Computer1 {
-            final int[] computer1 = new int[]{1};
-        }
-        class Computer2 {
-            final int[] computer2 = new int[]{2};
-        }
-        class Computer3 {
-            final int[] computer3 = new int[]{3};
-        }
-        class Computer4 {
-            final int[] computer4 = new int[]{4};
-        }
-        class Computer5 {
-            final int[] computer5 = new int[]{5};
-        }
-
-
-        for (int computer : computers) {
-            int i = 0;
-            while (i < computer) {
-                i++;
-            }
-            System.out.println(i + computer);
-        }
+    @Override
+    public String toString() {
+        return "Computer(" + "id" + id + ")";
     }
 }
 
